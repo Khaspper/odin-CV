7f975c1d4bafaefaf89088b063920731710e4bb2
Mark <brandonnarciso24@gmail.com>
2025-07-01 15:52:36 -0700

    Fresh Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline italic font-mono border-t-0">
-        Hello World
-      </h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
